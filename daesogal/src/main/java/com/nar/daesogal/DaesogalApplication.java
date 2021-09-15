00857f52d1064dd23f07365b02f2f581367908e3
wooh5 <wooh5678@gmail.com>
2021-09-15 16:51:54 +0900

    20210915

---
+++ daesogal/src/main/java/com/nar/daesogal/DaesogalApplication.java
@@ -0,0 +1,13 @@
+package com.nar.daesogal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaesogalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaesogalApplication.class, args);
+	}
+
+}
---
+++ daesogal/src/test/java/com/nar/daesogal/DaesogalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nar.daesogal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaesogalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
